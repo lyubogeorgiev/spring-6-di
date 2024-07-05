b11a49dc9bfd442619959fa1723ae29558150a2a
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-07-05 13:44:54 -0600

    Initial Commit

---
+++ spring6di/src/test/java/com/georgievl/spring6di/Spring6diApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgievl.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6diApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
